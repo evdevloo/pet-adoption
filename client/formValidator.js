cb87f2522b6564aa47af626d3540403b994ab8c7
Evert Devloo <evert.devloo@student.odisee.be>
2023-05-30 22:03:17 +0200

    added review begin template

---
+++ client/formValidator.js
@@ -0,0 +1,131 @@
+
+export class FormValidator {
+    constructor(form) {
+        this.form = form;
+        this.validators = [];
+        this.errors = [];
+
+        this.form.addEventListener('submit', (event) => this.onSubmit(event));
+    }
+
+    addValidator(validator) {
+        this.validators.push({
+            // spread de validator,
+            ...validator,
+            field: this.form.elements[validator.name]
+        })
+    }
+
+    validate(){
+
+        this.validators.forEach(validator => {
+
+
+            if (this.errors.includes(validator)){
+                return;
+            } else if (!validator.method(validator.field)){
+
+                this.errors.push(validator);
+            }
+        });
+
+        return this.errors.length === 0;
+
+    }
+
+    onSubmit(event) {
+        this.resetSummary();
+        this.removeInlineErrors();
+
+
+        if (!this.validate()){
+            event.preventDefault();
+            event.stopImmediatePropagation();
+            this.showSummary();
+            this.showInlineErrors();
+        }
+    }
+
+    createInlineError(error) {
+
+        const span = document.createElement('span');
+
+        span.className = 'field-error';
+        span.innerText = error.message;
+        span.id = `${error.name}-error`;
+
+        return span;
+    }
+
+    showInlineErrors() {
+
+        this.errors.forEach(i => {
+            const error = this.createInlineError(i);
+
+            if (i.field instanceof Node){
+
+
+                i.field.classList.add("invalid");
+                i.field.setAttribute('aria-invalid', 'true');
+                i.field.labels[0].appendChild(error);
+            } else if (i.field instanceof NodeList){
+                i.field.forEach(node => {
+                    node.classList.add("invalid");
+                    node.setAttribute('aria-invalid', 'true');
+                    node.setAttribute('aria-describedby', error.id);
+                })
+
+                const fieldset = i.field[0].closest('fieldset');
+                const legend = fieldset.querySelector('legend');
+
+                if (legend){
+                    legend.appendChild(error);
+                }
+            }
+        });
+    }
+
+    removeInlineErrors(){
+        // zoek en verwijder alle elementen **in het formulier**
+        // met de class `.field-error`
+        this.form.querySelectorAll('.field-error')
+            .forEach(element => element.remove());
+
+        this.form.querySelectorAll('.invalid')
+            .forEach(el => {
+                el.removeAttribute('aria-invalid');
+                el.removeAttribute('aria-describedby');
+                el.classList.remove('invalid');
+            });
+        this.errors = [];
+    }
+
+    createSummary(error) {
+
+        const li = document.createElement('li');
+
+        li.className = 'field-error';
+        li.innerText = error.message;
+        li.id = `${error.name}-error`;
+
+        return li;
+    }
+
+    showSummary() {
+
+        this.errors.forEach(error => {
+            const li = this.createSummary(error);
+            li.innerText = `!!!${li.innerText}!!!`
+            const summary = document.querySelector('.errorSummary ul');
+            summary.appendChild(li);
+        });
+
+    }
+
+    resetSummary() {
+
+    }
+
+
+
+}
---
+++ client/main.js
@@ -0,0 +1,78 @@
+"use strict";
+import {FormValidator} from './formValidator.js';
+
+
+(function() {
+
+    // selecteer je form
+    const form = document.querySelector('#myVerySpecialForm');
+    if (!form) {
+        return;
+    }
+    // wat als form undefined is?
+    // is het dan verstandig om verder te gaan? ... HINT!
+
+    // maak een nieuwe instantie van FormValidator
+    const validator = new FormValidator(form);
+
+    validator.addValidator({
+        // name verwijst naar het name attribute op het formulierveld
+        name: 'firstname',
+        // method ontvangt het inputveld als argument en returnt true of false
+        method: (field) => field.value.trim().length > 0,
+        message: 'Voornaam is een verplicht veld en werd niet ingevuld'
+    })
+
+    validator.addValidator({
+        // name verwijst naar het name attribute op het formulierveld
+        name: 'lastname',
+        // method ontvangt het inputveld als argument en returnt true of false
+        method: (field) => field.value.trim().length > 0,
+        message: 'familienaam is een verplicht veld en werd niet ingevuld'
+    })
+
+    validator.addValidator({
+        name: 'score',
+        method: field => field.value.trim().match(/^[0-9]+$/),
+        message: 'Je moet de review een score geven en het moet een getal zijn',
+    })
+
+    validator.addValidator({
+        name: 'privacy',
+        method: field => field.checked,
+        message: 'Privacy moet geaccepteerd worden'
+    })
+
+
+
+
+    form.addEventListener('submit', async function(event) {
+        // niet vergeten!
+        // anders: page refresh
+        event.preventDefault()
+
+        console.log('Submit gelukt! Geen errors!')
+
+        let res = await fetch("http://localhost:3000/api/v1/reviews", {
+            method: "POST",
+            body: JSON.stringify({
+                firstName: document.getElementById("firstname").value,
+                lastName: document.getElementById("lastname").value,
+                rating: document.getElementById("score").value,
+            }),
+            headers: {
+                "content-type": "application/json",
+            },
+        });
+
+        if (res.ok) {
+            document.location.href = "./reviews.html";
+        } else {
+            alert("Something went wrong while submitting the form!");
+        }
+
+        // herstel het formulier in zijn oorspronkelijke staat
+        // wat is this? 🤔
+    })
+})()
+
